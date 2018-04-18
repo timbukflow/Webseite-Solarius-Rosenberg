8024c908ec2a4f75d60b2ee7d6c4573b6ccd5e41
timbuk <ivoschwizer@gmail.com>
2018-04-18 08:57:37 +0200

    all good all fix

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ var prevScrollpos = window.pageYOffset;
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-        x.className += "responsive";
+        x.className += " responsive";
     } else {
         x.className = "topnav";
     }
