b6f0781a3123316911364818a6b33847c03fdbef
timbuk <ivoschwizer@gmail.com>
2018-04-24 08:54:36 +0200

    jquery nav fix and slide down

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,26 @@
-$(document).ready(function(){
-    $(".menu").click(function(){
+$(document).ready(function () {
+    
+    //Responsive Navigation
+    $(".menu").click(function () {
         $(".menu").css("display", "none");
         $(".topnav .navresp").css("display", "block");
         $(".close").css("display", "block");
     });
-    $(".close").click(function(){
+    $(".close").click(function () {
         $(".menu").css("display", "block");
         $(".topnav .navresp").css("display", "none");
         $(".close").css("display", "none");
     });
+    
+   
+    
 });
 
-//Menu by scroll big and small
-var prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
-        var currentScrollPos = window.pageYOffset;
-              if (prevScrollpos > currentScrollPos) {
-                  document.getElementById("nav-container").style.padding = "40px 5% 40px 5%";
-              } else {
-                  document.getElementById("nav-container").style.padding = "20px 5% 20px 5%";
-              }
-              prevScrollpos = currentScrollPos;
-            }
+ //Sroll resizing Navigation
+    $(document).on("scroll", function () {
+    if ($(document).scrollTop() > 20) {
+        $("#navcontainer").css("padding", "20px 5%"); 
+    } else{
+        $("#navcontainer").css("padding","40px 5%");
+    }
+    });
