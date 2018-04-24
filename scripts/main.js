a758f99d6537363beae532fdac585947b37d8e21
timbuk <ivoschwizer@gmail.com>
2018-04-24 16:33:55 +0200

    new site lage / miete

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,34 @@
 $(document).ready(function () {
     
+    $(window).resize(function () {
+    if ($(window).width() < 931) {
+        $(".topnav .navresp").css("display", "none");
+        $(".menu").show();
+        $(".close").hide();
+    } else {
+        $(".topnav .navresp").css("display", "inline-block");
+        $(".close").hide();
+    }
+    });
+    
     //Responsive Navigation
     $(".menu").click(function () {
-        $(".menu").css("display", "none");
+        $(".menu").hide();
         $(".topnav .navresp").css("display", "block");
-        $(".close").css("display", "block");
+        $(".close").show();
     });
     $(".close").click(function () {
-        $(".menu").css("display", "block");
+        $(".menu").show();
         $(".topnav .navresp").css("display", "none");
-        $(".close").css("display", "none");
+        $(".close").hide();
     });
     
-   
-    
-});
-
- //Sroll resizing Navigation
+   //Sroll resizing Navigation
     $(document).on("scroll", function () {
     if ($(document).scrollTop() > 20) {
         $("#navcontainer").css("padding", "20px 5%"); 
-    } else{
-        $("#navcontainer").css("padding","40px 5%");
-    }
+    } else {
+        $("#navcontainer").css("padding", "40px 5%"); }
     });
+    
+});
