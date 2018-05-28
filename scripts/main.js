c98b2fb53a4b625b4a041435b97a9d941a5065d5
Ivo-Schwizer <ivo@schwizerschlatter.com>
2018-05-28 16:02:26 +0200

    on. click funktion new

--- scripts/main.js
+++ scripts/main.js
@@ -2,25 +2,26 @@ $(document).ready(function () {
 
     //Responsive Navigation
     
-    $(".menu").click(function () {
-        $(".menu").hide();
-        $(".topnav .navresp").css("display", "block");
-        $(".close").css("display", "block");
+    $('.menu').on('click', () => {
+        $('.menu').hide();
+        $('.topnav .navresp').css('display', 'block');
+        $('.close').css('display', 'block');
     });
     
-    $(".close").click(function () {
-        $(".menu").show();
-        $(".topnav .navresp").hide();
-        $(".close").hide();
+     $('.close').on('click', () => {
+        $('.menu').show();
+        $('.topnav .navresp').hide();
+        $('.close').hide();
     });
+    
     
     //Sroll resizing Navigation
     
-    $(document).on("scroll", function () {
+    $(document).on('scroll', function () {
         if ($(document).scrollTop() > 10) {
-        $("#navcontainer").css("padding", "20px 5%"); 
+        $('#navcontainer').css('padding', '20px 5%'); 
     }   else {
-        $("#navcontainer").css("padding", "40px 5%"); 
+        $('#navcontainer').css('padding', '40px 5%'); 
     }
     });
     
